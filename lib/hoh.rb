ec86379aafa632265498a358518f29fe93d3421a
PianoFF <Fangfei1star@gmail.com>
2019-11-02 18:33:14 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,33 +4,55 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
+  bio_of_earh={
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category:{
+          label: "Order",
+          sub_category:{
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category:{
+                label: "Species",
+                 sub_category: nil 
+              }
+            }
+          }
+        }
+      }
+    }
+  }
   # Remember:
   #  Kingdom
   #  Phylum
